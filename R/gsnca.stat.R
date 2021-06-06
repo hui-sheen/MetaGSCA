cc1c4e9602a18b9e5c60908b594ad29ba26db441
Hui Yu <schena.yu@gmail.com>
2021-06-05 21:42:08 -0600

    Most updated R/Rd files

---
+++ R/gsnca.stat.R
@@ -0,0 +1,27 @@
+#' Test statistics of Gene Sets Net Correlation Analysis (GSNCA) 
+#'
+#' Derive distance statistics by solving eigenvector of two pairise correaltion matrix.
+#'
+#' @export
+#' @seealso [gsnca.p()] for the external function that calls on the current function and returns permutation p-statistic.
+#' @examples
+#' data(BRCA)
+#' N <- ncol(BRCA)
+#' n1 <- floor(N/2)
+#' objt1 <- t(BRCA[1:100,1:n1])
+#' objt2 <- t(BRCA[1:100,(n1+1):N])
+#' gStat.res <- gsnca.stat(objt1,objt2)
+#'
+#' @param objt1 dataset for condition 1, genes in columns
+#' @param objt2 dataset for condition 2, genes in columns
+#' @return L1-norm distance between two weight vectors
+gsnca.stat <- function(objt1,objt2,cor.method='pearson') {
+	cormat1 <- abs(cor(objt1, method = cor.method))
+	cormat2 <- abs(cor(objt2, method = cor.method))
+	e1 <- eigen(cormat1)        
+	e2 <- eigen(cormat2)
+	p1 <- abs(e1$vectors[, 1])
+	p2 <- abs(e2$vectors[, 1])
+	D_obs <- sum(abs(p1 * norm(matrix(p1)) - p2 * norm(matrix(p2))))
+	D_obs
+}
